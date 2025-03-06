56cfca55a2e3483f4552d02457ec375e1afe5187
Khaing Myat Mon Thein <khaing.mm.thein@daiichilife.com.mm>
2025-03-06 16:23:58 +0630

    Testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ const schema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Email is required'),
   password: yup.string()
     .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
+    .min(8, 'Password must be at least 8 characters!')
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character'),
 });
 
